9654d061400ea62b86594f94eee2ce70eee101a2
Reddogstone <thereddog@gmx.net>
2016-01-24 15:42:19 +0100

    Start with the first level again

--- game/screens/root.js
+++ game/screens/root.js
@@ -225,7 +225,7 @@ var RootScreen = function() {
 		while (true) {
 			yield TitleScreen();
 
-			for (var i = 9; i < levels.length; i++) {
+			for (var i = 0; i < levels.length; i++) {
 				var level = levels[i];
 				yield MainScreen(level);
 			}
